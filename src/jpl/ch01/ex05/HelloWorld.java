876266051bb6f73dd85cc08600eb432ab4a2fb31
euoni <euoni@users.noreply.github.com>
2016-09-28 13:44:31 +0900

    Add ex05

---
+++ src/jpl/ch01/ex05/HelloWorld.java
@@ -0,0 +1,9 @@
+package jpl.ch01.ex05;
+
+public class HelloWorld {
+	private static final String msg = "Hello, world";
+
+	public static void main(String[] args) {
+		System.out.println(msg);
+	}
+}
