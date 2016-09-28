d96fdcb6860e168189d27ec973945fbe8fe4e40c
euoni <euoni@users.noreply.github.com>
2016-09-28 14:53:03 +0900

    Add ex06

---
+++ test/jpl/ch01/ex06/FibonacciTest.java
@@ -0,0 +1,35 @@
+package jpl.ch01.ex06;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FibonacciTest {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+	}
+
+	@Test
+	public void test() {
+		Fibonacci.main(null);
+
+		String expected = String.join(
+			System.lineSeparator(),
+			new String[] { "Fibonacci", "1", "1", "2", "3", "5", "8", "13", "21", "34" }
+		);
+		assertEquals(expected + System.lineSeparator(), outContent.toString());
+	}
+}
