808fa0230017c7e2121b7f64cd03f0cf3f5d77cf
euoni <euoni@users.noreply.github.com>
2016-10-05 21:03:46 +0900

    Add ex11

---
+++ src/jpl/ch01/ex11/StringDemo.java
@@ -0,0 +1,12 @@
+package jpl.ch01.ex11;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String myName = "Ken";
+
+		myName = myName + " Arnold";
+		System.out.println("Name = " + myName);
+	}
+
+}
---
+++ test/jpl/ch01/ex11/StringDemoTest.java
@@ -0,0 +1,30 @@
+package jpl.ch01.ex11;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StringDemoTest {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+
+	@After
+	public void cleanUpStreams() {
+		System.setOut(null);
+	}
+
+	@Test
+	public void test() {
+		StringDemo.main(null);
+		assertEquals("Name = Ken Arnold" + System.lineSeparator(), outContent.toString());
+	}
+}
