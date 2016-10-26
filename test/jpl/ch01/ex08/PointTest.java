6eb8da24d8dd65526589857cf94fa573ace04795
euoni <euoni@users.noreply.github.com>
2016-10-27 05:25:42 +0900

    Use assertThat

--- test/jpl/ch01/ex08/PointTest.java
+++ test/jpl/ch01/ex08/PointTest.java
@@ -1,11 +1,11 @@
 package jpl.ch01.ex08;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class PointTest {
-
 	@Test
 	public void testClear() {
 		final Point p = new Point();
@@ -14,8 +14,8 @@ public class PointTest {
 
 		p.clear();
 
-		assertEquals(0, p.x, 0);
-		assertEquals(0, p.y, 0);
+		assertThat(p.x, is(0.));
+		assertThat(p.y, is(0.));
 	}
 
 	@Test
@@ -30,9 +30,9 @@ public class PointTest {
 		p3.x = 0;
 		p3.y = 1;
 
-		assertEquals(1, p1.distance(p2), 0);
-		assertEquals(1, p1.distance(p3), 0);
-		assertEquals(Math.sqrt(2), p2.distance(p3), 0);
+		assertThat(p1.distance(p2), is(1.));
+		assertThat(p1.distance(p3), is(1.));
+		assertThat(p2.distance(p3), is(Math.sqrt(2.)));
 	}
 
 	@Test
@@ -40,21 +40,20 @@ public class PointTest {
 		final Point p = new Point();
 		p.move(1, 2);
 
-		assertEquals(1, p.x, 0);
-		assertEquals(2, p.y, 0);
+		assertThat(p.x, is(1.));
+		assertThat(p.y, is(2.));
 	}
 
 	@Test
 	public void testMovePoint() {
 		final Point target = new Point();
-		target.x = 1;
-		target.y = 2;
+		target.x = 1.;
+		target.y = 2.;
 
 		final Point p = new Point();
 		p.move(target);
 
-		assertEquals(target.x, p.x, 0);
-		assertEquals(target.y, p.y, 0);
+		assertThat(p.x, is(target.x));
+		assertThat(p.y, is(target.y));
 	}
-
 }
--- test/jpl/ch01/ex16/MyUtilitiesTest.java
+++ test/jpl/ch01/ex16/MyUtilitiesTest.java
@@ -1,12 +1,11 @@
 package jpl.ch01.ex16;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class MyUtilitiesTest {
-
 	@Test(expected = BadDataSetException.class)
 	public void testGetDataSet() throws BadDataSetException {
 		final String setName = "not-exists";
@@ -14,10 +13,9 @@ public class MyUtilitiesTest {
 		try {
 			util.getDataSet(setName);
 		} catch (final BadDataSetException e) {
-			assertEquals("not-exists.dset", e.file);
-			assertNotNull(e.innerException);
+			assertThat(e.file, is("not-exists.dset"));
+			assertThat(e.innerException, not(nullValue()));
 			throw e;
 		}
 	}
-
 }
