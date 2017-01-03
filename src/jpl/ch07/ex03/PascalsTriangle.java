094f5347e0295acf366b9d0d53a5c935b91afe82
euoni <euoni@users.noreply.github.com>
2017-01-03 13:30:54 +0900

    Add ex03

---
+++ src/jpl/ch07/ex03/PascalsTriangle.java
@@ -0,0 +1,48 @@
+package jpl.ch07.ex03;
+
+import java.util.Collections;
+
+public class PascalsTriangle {
+	public static int[][] triangle(int n) {
+		final int[][] result = new int[n][];
+		for (int i = 0; i < n; i++) {
+			result[i] = new int[i + 1];
+			for (int j = 0; j < result[i].length; j++) {
+				result[i][j] = binomial(i, j);
+			}
+		}
+		return result;
+	}
+
+	public static void print(int[][] tri) {
+		final int n = tri.length;
+		final int w = (int) Math.log10(tri[n - 1][n / 2]) + 2;
+
+		for (int i = 0; i < n; i++) {
+			final int s = w * (n - i - 1) / 2;
+			System.out.print(String.join("", Collections.nCopies(s, " ")));
+			for (int j = 0; j < tri[i].length; j++) {
+				System.out.printf("% " + w + "d", tri[i][j]);
+			}
+			System.out.println("");
+		}
+	}
+
+	private static int binomial(int n, int k) {
+		if (k > n - k)
+			k = n - k;
+
+		int b = 1;
+		for (int i = 1, m = n; i <= k; i++, m--)
+			b = b * m / i;
+		return b;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("n=12");
+		print(triangle(12));
+
+		System.out.println("n=15");
+		print(triangle(15));
+	}
+}
