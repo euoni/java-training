8511f982c5488f8ea94f5531a67e3e4bb9ef7575
euoni <euoni@users.noreply.github.com>
2017-02-21 20:24:46 +0900

    Add ex01

---
+++ src/jpl/ch14/ch01/ThreadName.java
@@ -0,0 +1,7 @@
+package jpl.ch14.ch01;
+
+public class ThreadName {
+	public static void main(String[] args) {
+		System.out.println(Thread.currentThread().getName());
+	}
+}
---
+++ test/jpl/ch14/ch01/ThreadNameTest.java
@@ -0,0 +1,24 @@
+package jpl.ch14.ch01;
+
+import org.junit.Test;
+
+import jp.ne.sonet.ca2.yshibata.test.StdoutCapture;
+
+public class ThreadNameTest {
+	@Test
+	public void testThreadName() {
+		new ThreadName();
+		return;
+	}
+
+	@Test
+	public void testMain() {
+		final StdoutCapture sc = new StdoutCapture();
+		sc.start();
+
+		ThreadName.main(null);
+
+		sc.stop();
+		sc.assertEquals("main");
+	}
+}
