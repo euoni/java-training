2a1a869e2d13915fbe8fc3f4cf37c4f4254e649a
euoni <euoni@users.noreply.github.com>
2017-02-22 23:52:20 +0900

    Add ex03

---
+++ test/jpl/ch14/ex03/AddTest.java
@@ -0,0 +1,18 @@
+package jpl.ch14.ex03;
+
+import org.junit.Test;
+
+import jp.ne.sonet.ca2.yshibata.test.StdoutCapture;
+
+public class AddTest {
+	@Test
+	public void testMain() throws InterruptedException {
+		final StdoutCapture sc = new StdoutCapture();
+		sc.start();
+
+		Add.main(null);
+
+		sc.stop();
+		sc.assertEquals("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15");
+	}
+}
