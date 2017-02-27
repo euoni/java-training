f2dd8026c60bef9abebbce2379ca185cbce7fd7c
euoni <euoni@users.noreply.github.com>
2017-02-28 00:21:44 +0900

    Add ex01

---
+++ src/jpl/ch16/ex01/TypeDesc.java
@@ -0,0 +1,63 @@
+package jpl.ch16.ex01;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+
+public class TypeDesc {
+
+	public static void main(String[] args) {
+		final TypeDesc desc = new TypeDesc();
+		for (final String name : args) {
+			try {
+				final Class<?> startClass = Class.forName(name);
+				desc.printType(startClass, 0, basic);
+			} catch (final ClassNotFoundException e) {
+				System.err.println(e);
+			}
+		}
+	}
+
+	private final java.io.PrintStream out = System.out;
+
+	private static String[] basic = { "class", "interface", "enum", "annotation" },
+			supercl = { "extends", "implements" }, iFace = { null, "extends" };
+
+	private void printType(Type type, int depth, String[] labels) {
+		if (type == null)
+			return;
+
+		Class<?> cls = null;
+		if (type instanceof Class<?>)
+			cls = (Class<?>) type;
+		else if (type instanceof ParameterizedType)
+			cls = (Class<?>) ((ParameterizedType) type).getRawType();
+		else
+			throw new Error("Unexpected non-class type");
+
+		for (int i = 0; i < depth; i++)
+			out.print(" ");
+		final int kind = cls.isAnnotation() ? 3 : cls.isEnum() ? 2 : cls.isInterface() ? 1 : 0;
+		out.print(labels[kind] + " ");
+		out.print(cls.getCanonicalName());
+
+		final TypeVariable<?>[] params = cls.getTypeParameters();
+		if (params.length > 0) {
+			out.print('<');
+			for (final TypeVariable<?> param : params) {
+				out.print(param.getName());
+				out.print(", ");
+			}
+			out.println("\b\b>");
+		} else
+			out.println();
+
+		final Type[] interfaces = cls.getGenericInterfaces();
+		for (final Type iface : interfaces) {
+			printType(iface, depth + 1, cls.isInterface() ? iFace : supercl);
+		}
+
+		if (cls.getGenericSuperclass() != Object.class)
+			printType(cls.getGenericSuperclass(), depth + 1, supercl);
+	}
+}
---
+++ test/jpl/ch16/ex01/TypeDescTest.java
@@ -0,0 +1,42 @@
+package jpl.ch16.ex01;
+
+import org.junit.Test;
+
+import jp.ne.sonet.ca2.yshibata.test.StdoutCapture;
+
+public class TypeDescTest {
+	@Test
+	public void testMainObject() {
+		final StdoutCapture sc = new StdoutCapture();
+		sc.start();
+
+		TypeDesc.main(new String[] { "java.lang.Object" });
+
+		sc.stop();
+		sc.assertEquals("class java.lang.Object");
+	}
+
+	@Test
+	public void testMainHashMap() {
+		final StdoutCapture sc = new StdoutCapture();
+		sc.start();
+
+		TypeDesc.main(new String[] { "java.util.HashMap" });
+
+		sc.stop();
+		sc.assertEquals("class java.util.HashMap<K, V, \b\b>", " implements java.util.Map<K, V, \b\b>",
+				" implements java.lang.Cloneable", " implements java.io.Serializable",
+				" extends java.util.AbstractMap<K, V, \b\b>", "  implements java.util.Map<K, V, \b\b>");
+	}
+
+	@Test
+	public void testMainError() {
+		final StdoutCapture sc = new StdoutCapture();
+		sc.start();
+
+		TypeDesc.main(new String[] { "not.existsing.Class" });
+
+		sc.stop();
+		sc.assertEquals("");
+	}
+}
