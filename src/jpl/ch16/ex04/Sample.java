11e9c5f749385061d7066f1dabee88ab19e80a23
euoni <euoni@users.noreply.github.com>
2017-03-01 23:58:00 +0900

    Add ex04

---
+++ src/jpl/ch16/ex04/Sample.java
@@ -0,0 +1,9 @@
+package jpl.ch16.ex04;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Sample
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Sample {
+}
